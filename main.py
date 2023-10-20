b60fbe48f9d8a5c1e8594f748e82c1ed7d4534ee
itz-amethyst <pransermi@gmail.com>
2023-10-20 22:03:09 +0330

    feat: Added some api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Todo
 
 app = FastAPI()
 
@@ -15,15 +16,38 @@ async def get_todos():
 
 
 # Get Single Todo
+@app.get('/todos/{todo_id}')
+async def get_todo_by_id(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return{"message": "No Data Found"}
 
 
 
 # Create Todo
-
+@app.post('/todos')
+async def create_todo(todo: Todo):
+    todos.append(todo)
+    return{"message": "Sucessfully Created !"}
 
 
 # Update Todo
-
+@app.put('/todos/{todo_id}')
+async def update_todo(todo_id: int, todo_new: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_new.item
+            return {"tood": todo}
+    return{"message": "No Data Found"}
 
 
 # Delete Todo
+@app.delete('/todos/{todo_id}')
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Operation Complete"}
+    return{"message": "No Data Found"}
